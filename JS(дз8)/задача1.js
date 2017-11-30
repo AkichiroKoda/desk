c21be0fe12f0926a9d73cd42df6ee4dad5c7c706
AkichiroKoda <akodadgm@gmail.com>
2017-11-30 11:23:12 +0300

    hw8

---
+++ JS(дз8)/задача1.js
@@ -0,0 +1,21 @@
+;(function(){
+    function clock(){
+    var date = new Date();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var second = date.getSeconds();
+   
+    if(hour < 10){
+        hour = "0" + hour;
+    }
+    if(minute < 10){
+        minute = "0" + minute;
+    }
+    if(second < 10){
+        second = "0" + second;
+    }
+    var setClock = document.getElementById("set_clock");
+    setClock.innerHTML = hour + " : " + minute + " : " + second;
+    setTimeout("clock()", 1000);
+}
+})(); 
