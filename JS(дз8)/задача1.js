cf678345fad621c9d1a31c26a3ac399092efda54
AkichiroKoda <akodadgm@gmail.com>
2017-12-05 10:52:22 +0300

    hw8

--- JS(дз8)/задача1.js
+++ JS(дз8)/задача1.js
@@ -1,10 +1,14 @@
-
-    ;(function clock(){
+;(function clock(){
     var date = new Date();
     var hour = date.getHours();
     var minute = date.getMinutes();
     var second = date.getSeconds();
-   
+    function getRand(min, max){
+        return Math.floor(Math.random()*(max-min + 1) + min);
+    }
+    function getRandStyle(){
+        return "color: rgb(" + getRand(0,255) + ", " + getRand(0,255) + ", " + getRand(0,255) +"); background-color: rgb(" + getRand(0,255) + ", " + getRand(0,255) + ", " + getRand(0,255) +"); font-size: 50px;";
+    }
     if(hour < 10){
         hour = "0" + hour;
     }
@@ -14,8 +18,15 @@
     if(second < 10){
         second = "0" + second;
     }
-    var setClock = document.getElementById("set_clock");
-    setClock.innerHTML = hour + " : " + minute + " : " + second;
-    setTimeout("clock()", 1000);
+    var clockHour = document.getElementById("hour");
+    var clockMinute = document.getElementById("minute");
+    var clockSecond = document.getElementById("second");
+    clockHour.innerHTML = hour;
+    clockHour.style = getRandStyle();
+    clockMinute.innerHTML = minute;
+    clockMinute.style = getRandStyle();
+    clockSecond.innerHTML = second;
+    clockSecond.style = getRandStyle();
+    setInterval(clock, 1000);
 })();
  
